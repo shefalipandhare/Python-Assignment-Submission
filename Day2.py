e2e283876b3ed2e6a009930096de1402aa2d84d5
shefalipandhare <98814209+shefalipandhare@users.noreply.github.com>
2022-02-02 18:49:54 +0530

    Add files via upload

---
+++ Day2.py
@@ -0,0 +1,11 @@
+lottery = "Hello coders"
+# 'h' or 'e' or 'l' or 'l' or ' o' or 'c' or 'd'
+
+print(lottery[0])
+print("Choose correct character of the word '",lottery,"'to win the lottery")
+inputs = input(" ").lower()
+
+if inputs == lottery[0] or inputs ==lottery[1] or inputs == lottery[4] or inputs == lottery[6] or inputs == lottery[8]:
+ print("congratulation , you have won the lottery")
+else:
+ print("sorry, you didn't win a lottery , try next time!")
