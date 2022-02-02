c93498ae399a6453473dda6ba62a44d322652dc1
shefalipandhare <98814209+shefalipandhare@users.noreply.github.com>
2022-02-02 18:51:53 +0530

    Update Day2.py

--- Day2.py
+++ Day2.py
@@ -6,6 +6,6 @@ print("Choose correct character of the word '",lottery,"'to win the lottery")
 inputs = input(" ").lower()
 
 if inputs == lottery[0] or inputs ==lottery[1] or inputs == lottery[4] or inputs == lottery[6] or inputs == lottery[8]:
- print("congratulation , you have won the lottery")
+ print("congratulations , you have won the lottery!")
 else:
  print("sorry, you didn't win a lottery , try next time!")
